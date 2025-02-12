7480ba12ac097cdf923bd5f19416ceda4fa7ecd8
marco-bernal <bernal.marco.a.84@gmail.com>
2025-02-12 17:28:19 -0600

    LTTS-3 - Add dropdown with available models

--- README.md
+++ README.md
@@ -34,3 +34,32 @@ Generates speech from text with non-robotic voices.
 
 ### Reference:
 https://www.youtube.com/watch?v=EyzRixV8s54
+
+#### Tested models:
+* 10,tts_models/en/ek1/tacotron2  -> <b>Doesn't work!</b>
+* 11,tts_models/en/ljspeech/tacotron2-DDC -> <b>Works well!</b>
+* 12,tts_models/en/ljspeech/tacotron2-DDC_ph
+* 13,tts_models/en/ljspeech/glow-tts
+* 14,tts_models/en/ljspeech/speedy-speech
+* 15,tts_models/en/ljspeech/tacotron2-DCA
+* 16,tts_models/en/ljspeech/vits
+* 17,tts_models/en/ljspeech/vits--neon
+* 18,tts_models/en/ljspeech/fast_pitch
+* 19,tts_models/en/ljspeech/overflow
+* 20,tts_models/en/ljspeech/neural_hmm
+* 21,tts_models/en/vctk/vits
+* 22,tts_models/en/vctk/fast_pitch
+* 23,tts_models/en/sam/tacotron-DDC
+* 24,tts_models/en/blizzard2013/capacitron-t2-c50
+* 25,tts_models/en/blizzard2013/capacitron-t2-c150_v2
+* 26,tts_models/en/multi-dataset/tortoise-v2
+* 27,tts_models/en/jenny/jenny
+* 32,tts_models/uk/mai/glow-tts
+* 33,tts_models/uk/mai/vits
+* v3,vocoder_models/en/ek1/wavegrad
+* v4,vocoder_models/en/ljspeech/multiband-melgan
+* v5,vocoder_models/en/ljspeech/hifigan_v2
+* v6,vocoder_models/en/ljspeech/univnet
+* v7,vocoder_models/en/blizzard2013/hifigan_v2
+* v8,vocoder_models/en/vctk/hifigan_v2
+* v9,vocoder_models/en/sam/hifigan_v2
--- main.py
+++ main.py
@@ -1,54 +1,43 @@
 import torch
 from TTS.api import TTS
 import gradio as gr
+import pandas as pd
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# TODO: Create dictionary or file to load available english models
-# Available english models:
-#  10: tts_models/en/ek1/tacotron2
-#  11: tts_models/en/ljspeech/tacotron2-DDC
-#  12: tts_models/en/ljspeech/tacotron2-DDC_ph
-#  13: tts_models/en/ljspeech/glow-tts
-#  14: tts_models/en/ljspeech/speedy-speech
-#  15: tts_models/en/ljspeech/tacotron2-DCA
-#  16: tts_models/en/ljspeech/vits
-#  17: tts_models/en/ljspeech/vits--neon
-#  18: tts_models/en/ljspeech/fast_pitch [already downloaded]
-#  19: tts_models/en/ljspeech/overflow
-#  20: tts_models/en/ljspeech/neural_hmm
-#  21: tts_models/en/vctk/vits
-#  22: tts_models/en/vctk/fast_pitch
-#  23: tts_models/en/sam/tacotron-DDC
-#  24: tts_models/en/blizzard2013/capacitron-t2-c50
-#  25: tts_models/en/blizzard2013/capacitron-t2-c150_v2
-#  26: tts_models/en/multi-dataset/tortoise-v2
-#  27: tts_models/en/jenny/jenny
-#
-#  32: tts_models/uk/mai/glow-tts
-#  33: tts_models/uk/mai/vits
-#
-#  3: vocoder_models/en/ek1/wavegrad
-#  4: vocoder_models/en/ljspeech/multiband-melgan
-#  5: vocoder_models/en/ljspeech/hifigan_v2 [already downloaded]
-#  6: vocoder_models/en/ljspeech/univnet
-#  7: vocoder_models/en/blizzard2013/hifigan_v2
-#  8: vocoder_models/en/vctk/hifigan_v2
-#  9: vocoder_models/en/sam/hifigan_v2
+def load_models(file_path):
+    return pd.read_csv(file_path)
 
 # TODO: Validate if the output folder exists, else create it.
-def generate_audio(text):
-    tts = TTS(model_name='tts_models/en/ljspeech/glow-tts').to(device) # Replace model name from dictionary
-    tts.tts_to_file(text=text, file_path="outputs/output.wav") # User should choose the file name
+def generate_audio(model, text):
+    tts = TTS(model_name = model).to(device)
+    tts.tts_to_file(text = text, file_path="outputs/output.wav")
     return "outputs/output.wav" # Delete
-# Improve UI
-gui = gr.Interface(
-    fn=generate_audio,
-    inputs=[gr.Text(label="Text"),],
-    outputs=[gr.Audio(label="Audio"),]
-)
+
+# TODO: Improve UI
+def launch_interface(models_df):
+    interface = gr.Interface( #TODO: Add title and bar title
+        fn = generate_audio,
+        inputs = [
+            gr.Dropdown(choices = list(models_df['name']), label="Available english models"),
+            gr.Text(label="Text") #TODO: Change size to a taller one
+        ],
+        outputs = [
+            gr.Audio(label="Audio") #TODO: Remove Flag button
+        ]
+    )
+
+    interface.launch()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    gui.launch()
+    # Load models
+    models_file_path = 'datasets/english_models.csv'
+    english_models = load_models(models_file_path)
+    #TODO: Test all the models and update the README file accordingly. Once finished, remove unusable models from the csv file
+
+    # print('Available models: ', english_models)
+    #Launch UI
+    launch_interface(english_models)
+
